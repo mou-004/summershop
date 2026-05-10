27ba2e2d081c50cfedd52faa1396b9eaa92c629d
Halima Akther <halimaakthermou004@gmail.com>
2026-05-11 00:30:33 +0600

    Add type annotation to middleware request parameter

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 export const runtime = "edge";
 
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(req) {
+export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
